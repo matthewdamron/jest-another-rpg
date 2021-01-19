39885fd8331171ef25a4e04bfd65cae2fc7e55e7
Matthew Damron <mattdamron@msn.com>
2021-01-19 16:57:14 -0700

    finished up the player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,7 +3,6 @@ jest.mock('../lib/Potion');
 // console.log(new Potion);
 
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Matt');
@@ -13,4 +12,23 @@ test('creates a player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
-})
+});
+
+test("gets player's stats as an oject", () => {
+    const player = new Player('Matt');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns fasle', () => {
+    const player = new Player('Matt');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
