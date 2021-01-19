c1e35dcc23b318b44aa5b316889f2cb79fa220cd
Matthew Damron <mattdamron@msn.com>
2021-01-19 16:12:19 -0700

    updated player test

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,9 @@
-const { expect } = require('@jest/globals');
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion');
+// console.log(new Potion);
+
 const Player = require('../lib/Player');
+const { expect } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Matt');
@@ -8,4 +12,5 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 })
